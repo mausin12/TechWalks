ebbea053c21820f396bd7d9ef070647b38f87fcd
Mausin Khan <mausin12@gmail.com>
2026-02-03 17:30:56 +0530

    feat: add Regions controller

---
+++ TechWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll(string? filterOn, string? filterQuery)
+        {
+            return Ok();
+        }
+    }
+}
