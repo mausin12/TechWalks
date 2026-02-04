5c076f95d96e44e45845d23cf84a72936b5e52f1
Mausin Khan <mausin12@gmail.com>
2026-02-04 16:50:18 +0530

    feat(mapping): add Automapper profiles

---
+++ TechWalks.API/Mappings/AutoMapperProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TechWalks.API.Models.Domain;
+using TechWalks.API.Models.Dto;
+
+namespace TechWalks.API.Mappings
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Region, RegionDto>().ReverseMap();
+        }
+    }
+}
