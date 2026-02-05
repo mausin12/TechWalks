164d374d01372ae4e48216bf653bc327a841e60c
Mausin Khan <mausin12@gmail.com>
2026-02-05 14:53:47 +0530

    chore(db): add seed data for region and difficulty

--- TechWalks.API/Migrations/20260205091409_SeedDifficultyAndRegionData.Designer.cs
+++ TechWalks.API/Migrations/20260205091409_SeedDifficultyAndRegionData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechWalks.API.Data;
 
@@ -11,9 +12,11 @@ using TechWalks.API.Data;
 namespace TechWalks.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260205091409_SeedDifficultyAndRegionData")]
+    partial class SeedDifficultyAndRegionData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
