c805e48e6eab9a07accb3072284a19151ee09ad6
Mausin Khan <mausin12@gmail.com>
2026-02-08 17:18:15 +0530

    chore(auth): add seed data for roles

--- TechWalks.API/Data/AuthDbContext.cs
+++ TechWalks.API/Data/AuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace TechWalks.API.Data
@@ -8,6 +9,34 @@ namespace TechWalks.API.Data
         public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "3847dd7c-dc3c-49b7-aed4-5fd7b0352f42";
+            var writerRoleId = "8669fe84-adff-4dbc-9ef4-166c4f13abed";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id=readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name="Reader",
+                    NormalizedName= "Reader".ToUpper()
+                },
+                 new IdentityRole
+                {
+                    Id=writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name="Writer",
+                    NormalizedName= "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
