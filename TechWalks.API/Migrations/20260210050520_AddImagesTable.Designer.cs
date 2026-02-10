a176df0b002fab5c75e87c2729a695ee54ce501f
Mausin Khan <mausin12@gmail.com>
2026-02-10 10:43:50 +0530

    chore(db): add database migration for Images

--- TechWalks.API/Migrations/20260210050520_AddImagesTable.Designer.cs
+++ TechWalks.API/Migrations/20260210050520_AddImagesTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechWalks.API.Data;
 
@@ -11,9 +12,11 @@ using TechWalks.API.Data;
 namespace TechWalks.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260210050520_AddImagesTable")]
+    partial class AddImagesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
