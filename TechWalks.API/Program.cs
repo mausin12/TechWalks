5ddc80f8edca224bcfb6f2b5037a405b3b25adf2
Mausin Khan <mausin12@gmail.com>
2026-02-10 17:15:20 +0530

    feat(files): enable serving uploaded images via static files middleware

--- TechWalks.API/Program.cs
+++ TechWalks.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using TechWalks.API.Data;
@@ -75,6 +76,12 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 
 app.UseAuthorization();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),"Images")),
+    RequestPath = "/Images"
+});
 
 app.MapControllers();
 
